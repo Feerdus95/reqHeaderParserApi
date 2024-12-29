1084853774bf5b1eb7e204f7c7f1608e127b7bb2
Feerdus95 <ferdus95.fv@gmail.com>
2024-12-29 03:12:22 -0300

    one more try

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 // Enable CORS
 app.use(cors({ optionsSuccessStatus: 200 }));
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // API endpoint
 app.get("/api/whoami", function (req, res) {
@@ -18,7 +19,7 @@ app.get("/api/whoami", function (req, res) {
 
 // Root route
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 // Listen on port set by environment variable or default to 3000
